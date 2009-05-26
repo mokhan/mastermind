c47b71e4c3a0fe18d2eb705bd0e9b7c33e6b2e92
mo khan <mo@mokhan.ca>
2009-05-26 14:29:57 -0600

    started working on initial design

--- features/step_definitions/mastermind.rb
+++ features/step_definitions/mastermind.rb
@@ -2,3 +2,13 @@
 
 Given /^I am not yet playing$/ do
 end
+
+When /^I start a new game$/ do
+	@messenger = StringIO.new
+	Mastermind::Game.new(@messenger)
+	game.start
+end
+
+Then /^the game should say "(.$)"$/ do |message|
+	@messenger.string.split("\n").should include(message)
+end
